f2025f243146ef95d669b5cc3296e0f31b9f8fe4
AleSantoni <alesantoni31@gmail.com>
2026-05-26 18:29:43 -0300

    (feat) "Admin centro medico "

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CentroMedico
+
+
+@admin.register(CentroMedico)
+class CentroMedicoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nombre',
+        'direccion',
+        'telefono',
+        'email',
+        'activo',
+    )
+
+    search_fields = (
+        'nombre',
+        'direccion',
+    )
+
+    list_filter = (
+        'activo',
+    )
+
+    ordering = (
+        'nombre',
+    )
