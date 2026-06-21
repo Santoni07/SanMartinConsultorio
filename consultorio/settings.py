3304eeb42b467d707f284bd497eb247fcf8ccb26
AleSantoni <alesantoni31@gmail.com>
2026-06-21 11:58:34 -0300

    (feat)agegamos variables https

--- consultorio/settings.py
+++ consultorio/settings.py
@@ -192,3 +192,18 @@ else:
    pass
 
 
+# HTTPS / PRODUCCION
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_PROXY_SSL_HEADER = (
+    ('HTTP_X_FORWARDED_PROTO', 'https')
+)
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://app.centromedicosanmartin.ar',
+]
