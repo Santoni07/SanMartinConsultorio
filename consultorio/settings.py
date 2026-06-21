cdcb32d426408c0bb157f1134621355e57f97408
AleSantoni <alesantoni31@gmail.com>
2026-06-21 07:49:51 -0300

    (feat) Cambios en el settings para el despliegue

--- consultorio/settings.py
+++ consultorio/settings.py
@@ -26,9 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-if##f6q2py9_6fih+1^xe*a^7=47bcubuhs=&*vky4m(=dccuv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'app.centromedicosanmartin.ar',
+    'consultorioSanMartin.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -161,6 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
