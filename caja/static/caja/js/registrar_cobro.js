d6881ac89c59e2030f317fcd80843ec8ce616b5d
AleSantoni <alesantoni31@gmail.com>
2026-07-07 09:07:56 -0300

    (feat) Separar javascript de registrar cobro

---
+++ caja/static/caja/js/registrar_cobro.js
@@ -0,0 +1,12 @@
+/*
+========================================================
+ Centro Médico San Martín
+ Registrar Cobro
+========================================================
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Registrar Cobro inicializado.");
+
+});
